db673f15df460331cd63c7f95dbecfc19c0bda78
weenta <644732319@qq.com>
2017-10-09 17:36:02 +0800

    zepto.init 函数

--- 99test.js
+++ 99test.js
@@ -10,6 +10,6 @@ var num = (function(){
 
 window.$ === undefined && (window.$ = Zepto)
 
-var z = { a:'abc',b:1};
+var s= { a:'abc',b:1};
 window.s === undefined && alert(123)	// 执行123
 window.s === undefined || alert(123)	// 阻断效应 不执行123
